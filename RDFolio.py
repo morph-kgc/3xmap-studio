8af280ba984e0a5c4aa3b923a1c110a59444caad
arenasg-paloma <paloma_arenas@hotmail.com>
2025-08-17 20:33:01 +0200

    pages folder renamed for case sensitivity

--- RDFolio.py
+++ RDFolio.py
@@ -1,9 +1,15 @@
 import streamlit as st
 
-st.set_page_config(page_title="RDFocus", layout="wide")
 
-st.sidebar.title("Menu")
-st.sidebar.write("Select a page")
+st.set_page_config(
+    page_title="🌌 RDFolio",
+    layout="wide"
+)
 
-st.title("Welcome to RDFolio")
-st.write("Use sidebar to work with existing or new graph")
+# st.sidebar.title("Menu")
+# st.sidebar.write("Select a page")
+
+
+
+st.title("Welcome to 🌌 RDFolio")
+st.write("Use sidebar to work with existing or new mapping")
