dc86a4661b69c7e3a597ce7d3efd50d6c4035357
arenasg-paloma <paloma_arenas@hotmail.com>
2025-10-09 20:57:29 +0200

    Re-added ontology tags

--- utils.py
+++ utils.py
@@ -930,6 +930,22 @@ def get_ontology_human_readable_name(g, source_link=None, source_file=None):
 #___________________________________________________________________________________
 
 #___________________________________________________________________________________
+#Function to get the human-readable name of an ontology
+def get_ontology_tag(g_label):
+
+    g = st.session_state["g_ontology_components_dict"][g_label]
+    g_intology_label = get_ontology_human_readable_name(g)
+    g_ontology_iri = next(g.subjects(RDF.type, OWL.Ontology), None)
+
+
+    if g_ontology_iri:
+        prefix = g.namespace_manager.compute_qname(g_ontology_iri)[0]
+        return prefix
+
+    return g_ontology_label[:4]
+#___________________________________________________________________________________
+
+#___________________________________________________________________________________
 #Function to get all allowed formats for ontology files
 def get_g_ontology_file_formats_dict():
 
