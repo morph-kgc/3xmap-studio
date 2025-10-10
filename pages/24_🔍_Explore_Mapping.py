27ef855439c364ad2aac66373b96586cde31a5cd
arenasg-paloma <paloma_arenas@hotmail.com>
2025-10-10 20:17:35 +0200

    Fav icon and new headers

--- pages/24_🔍_Explore_Mapping.py
+++ pages/24_🔍_Explore_Mapping.py
@@ -10,10 +10,10 @@ from rdflib.namespace import RDF, RDFS, DC, DCTERMS, OWL, XSD
 # Config-----------------------------------
 if "dark_mode_flag" not in st.session_state or not st.session_state["dark_mode_flag"]:
     st.set_page_config(page_title="3Xmap Studio", layout="wide",
-        page_icon="logo/fav-icon.png")
+        page_icon="logo/fav_icon.png")
 else:
     st.set_page_config(page_title="3Xmap Studio", layout="wide",
-        page_icon="logo/fav-icon-inverse.png")
+        page_icon="logo/fav_icon_inverse.png")
 
 # Header-----------------------------------
 dark_mode = False if "dark_mode_flag" not in st.session_state or not st.session_state["dark_mode_flag"] else True
