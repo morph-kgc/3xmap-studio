eb8c5b8473cfb2cc510514149a503edc8d8b1499
EmidioStani <emidiostani@gmail.com>
2025-10-23 16:27:03 +0200

    fix quote

--- pages/23_🏗️_Build_Mapping.py
+++ pages/23_🏗️_Build_Mapping.py
@@ -116,7 +116,7 @@ if "pom_deleted_ok_flag" not in st.session_state:
 def save_tm_w_existing_ls():
     # add triples___________________
     NS = st.session_state["structural_ns"][1]
-    tm_iri = NS[f"{st.session_state["tm_label"]}"]  # change so that is can be defined by user
+    tm_iri = NS[f"{st.session_state['tm_label']}"]  # change so that is can be defined by user
     NS = st.session_state["structural_ns"][1]
     ls_iri =  NS[f"{existing_ls}"]   # idem ns
     st.session_state["g_mapping"].add((tm_iri, RML.logicalSource, ls_iri))    #bind to logical source
@@ -130,7 +130,7 @@ def save_tm_w_existing_ls():
 def save_tm_w_tabular_ls():
     # add triples__________________
     NS = st.session_state["structural_ns"][1]
-    tm_iri = NS[f"{st.session_state["tm_label"]}"]
+    tm_iri = NS[f"{st.session_state['tm_label']}"]
     ds_filename = ds_file.name
     if ls_label:
         NS = st.session_state["structural_ns"][1]
@@ -169,7 +169,7 @@ def save_tm_w_query():
         jdbc_str = f"jdbc:mariadb://{host}:{port}/{database}"
     # add triples__________________
     NS = st.session_state["structural_ns"][1]
-    tm_iri = NS[f"{st.session_state["tm_label"]}"]
+    tm_iri = NS[f"{st.session_state['tm_label']}"]
     if ls_label:
         NS = st.session_state["structural_ns"][1]
         ls_iri = NS[f"{ls_label}"]
@@ -200,7 +200,7 @@ def save_tm_w_table_name():
         jdbc_str = f"jdbc:mariadb://{host}:{port}/{database}"
     # add triples__________________
     NS = st.session_state["structural_ns"][1]
-    tm_iri = NS[f"{st.session_state["tm_label"]}"]
+    tm_iri = NS[f"{st.session_state['tm_label']}"]
     if ls_label:
         NS = st.session_state["structural_ns"][1]
         ls_iri = NS[f"{ls_label}"]
@@ -2874,7 +2874,7 @@ with tab4:
             col1a, col1b = st.columns([1.5,1])
         with col1a:
             clean_g_mapping_checkbox = st.checkbox(
-            f"🔒 I am sure I want to clean mapping {st.session_state["g_label"]}",
+            f"🔒 I am sure I want to clean mapping {st.session_state['g_label']}",
             key="key_clean_g_mapping_checkbox")
         if clean_g_mapping_checkbox:
             with col1a:
